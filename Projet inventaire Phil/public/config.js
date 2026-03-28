e4e3c132ddb079d11f331eaf1b07369a453845b7
ok... <wgadouabillette@gmail.com>
2026-03-27 21:39:03 -0400

    Add files via upload

---
+++ Projet inventaire Phil/public/config.js
@@ -0,0 +1,7 @@
+// xAI Grok API key injected for frontend (never commit this file)
+// Value copied from your local .env (XAI_API_KEY)
+window.XAI_API_KEY = "xai-cVcMCPNC5Wi7GgXOEgaFami8qrij8YW5hYZ6jKRQJLiEZeADKNd9fWIOxciqXYwplB1KHYMrtSbFtxQv";
+
+// eBay OAuth : URL de la Cloud Function (sans slash final), ex. :
+// https://us-central1-phil-b1d11.cloudfunctions.net/ebayOAuth
+window.EBAY_OAUTH_BASE = 'https://us-central1-phil-b1d11.cloudfunctions.net/ebayOAuth';
